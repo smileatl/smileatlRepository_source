270398c99d60cb87dc3f8fe1f7f9c210d8ac3547
Lin Sonelei <1355484300@qq.com>
2023-06-01 21:10:05 +0800

    change

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -32,14 +32,14 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       { text: '首页', link: '/' },
       {
         text: '工作',
-        link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+        link: '/work/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
           // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
           {
             text: '编程语言',
             items: [
-              { text: 'C', link: '/pages/8143cc480faf9a11/' },
-              { text: 'C++', link: '/language/C++/myC++' },
+              { text: 'C', link: '/work/language/cpp/test/' },
+              { text: 'C++', link: '/work/language/cpp/myCpp/' },
               { text: 'python', link: '/pages/8143cc480faf9a11/' },
               { text: 'Java', link: '/pages/8143cc480faf9a11/' },
               { text: '脚本语言', link: '/pages/8143cc480faf9a11/' },
