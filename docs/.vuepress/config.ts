ea71e9c9b65aab33d585b88510b774dc830eef35
Lin Sonelei <1355484300@qq.com>
2023-06-10 20:22:02 +0800

    change

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,7 @@ import dayjs from 'dayjs'
 import baiduCode from './config/baiduCode' // 百度统计hm码
 // import htmlModules from './config/htmlModules' // 自定义插入的html块
 
-const DOMAIN_NAME = 'xugaoyi.com' // 域名 (不带https)
+const DOMAIN_NAME = 'smileatl.github.io' // 域名 (不带https)
 const WEB_SITE = `https://${DOMAIN_NAME}` // 网址
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
@@ -291,36 +291,36 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
     ['fulltext-search'],
-
-    // // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
-    // [
-    //   'thirdparty-search',
-    //   {
-    //     thirdparty: [
-    //       // {
-    //       //   title: '在MDN中搜索',
-    //       //   frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-    //       //   behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-    //       // },
-    //       // {
-    //       //   title: '在Runoob中搜索',
-    //       //   frontUrl: 'https://www.runoob.com/?s=',
-    //       // },
-    //       // {
-    //       //   title: '在Vue API中搜索',
-    //       //   frontUrl: 'https://cn.vuejs.org/v2/api/#',
-    //       // },
-    //       {
-    //         title: '在Bing中搜索',
-    //         frontUrl: 'https://cn.bing.com/search?q=',
-    //       },
-    //       {
-    //         title: '通过百度搜索本站的',
-    //         frontUrl: `https://www.baidu.com/s?wd=site%3A${DOMAIN_NAME}%20`,
-    //       },
-    //     ],
-    //   }
-    // ],
+   
+    // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
+    [
+      'thirdparty-search',
+      {
+        thirdparty: [
+          // {
+          //   title: '在MDN中搜索',
+          //   frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+          //   behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+          // },
+          // {
+          //   title: '在Runoob中搜索',
+          //   frontUrl: 'https://www.runoob.com/?s=',
+          // },
+          // {
+          //   title: '在Vue API中搜索',
+          //   frontUrl: 'https://cn.vuejs.org/v2/api/#',
+          // },
+          {
+            title: '在Bing中搜索',
+            frontUrl: 'https://cn.bing.com/search?q=',
+          },
+          {
+            title: '通过百度搜索本站的',
+            frontUrl: `https://www.baidu.com/s?wd=site%3A${DOMAIN_NAME}%20`,
+          },
+        ],
+      }
+    ],
 
     [
       'one-click-copy', // 代码块复制按钮
